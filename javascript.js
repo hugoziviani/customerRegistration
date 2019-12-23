89b898dba8ae93fa786742f4270a2653e1f301f5
Hugo Ziviani <hugo_ziviani@hotmail.com>
2019-12-23 20:18:29 -0300

    Incluindo um projeto no github

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/* Arquivo JavaScript */
+
+function validacao() {
+	if(document.for.Nome.value=="")
+		alert("Por favor, preencha o campo Nome.");
+	    document.form.Nome.focus();
+	    return false;
+	}
+}
+
+if(document.for.CPF.value,length < 11) {
+   alert("A senha deve conter no mínimo 8 caracteres.");
+   document.form.senha.focus();
+   retur false;
+  } 
+}
