3caf94b604d00bbbf94a54a9b660654f53fe4da3
Hugo Ziviani <hz0@Hugos-MacBook-Air.local>
2020-01-08 23:33:42 -0300

    corrigindo arquivos

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 
 app = Flask (__name__)
-
-
-
 customersList = [{
 
 		"id"   : 1,
---
+++ config.py
@@ -0,0 +1,6 @@
+
+host = 'localhost'
+port = '3306'
+user = 'hugo'
+password = 'pwd.123'
+database = 'registers_db'
