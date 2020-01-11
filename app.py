fefd2643c30851a241c538462305f6ff20df0ae7
Hugo Ziviani <hz0@Hugos-MacBook-Air.local>
2020-01-11 12:51:00 -0300

    Transformacao de dados em JSON envio e consulta ao Banco

--- app.py
+++ app.py
@@ -1,71 +1,71 @@
 from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import config
+
+
 
 app = Flask (__name__)
-customersList = [{
 
-		"id"   : 1,
-		"name" : "Nat",
-		"lang" : "javaScript" 
-	},
-	{
-		"id"   : 2,
-		"name" : "Joaquin",
-		"lang" : "CSharp" 
-	},
-	{
-		"id"   : 3,
-		"name" : "Harbor",
-		"lang" : "Assembly" 
-	},
-	{
-		"id"   : 4,
-		"name" : "Philip",
-		"lang" : "c++" 
-	}
-]
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+app.config['SQLALCHEMY_DATABASE_URI']= config.connectionString
+
+
+class CustomerModel(db.Model):
+    __tablename__ = 'customersTable' #nome da tabela dele no banco
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthDay = db.Column(db.String)
+    generalRegistry = db.Column(db.String)
+    address = db.Column(db.String)
+    phoneNumber = db.Column(db.String)
+    historic = db.Column(db.Boolean)
+    
+    def __repr__(self):
+        return ("<Customer(name=" + self.name 
+		+ ", birthDay=" + self.birthDay 
+		+ ", generalRegistry=" + self.generalRegistry 
+		+ ", address=" + self.address 
+		+ ", phoneNumber=" + self.phoneNumber 
+		+ ", historic=" + self.historic + ">")
+class CustomerSchema(ma.ModelSchema):
+	class Meta:
+		model = CustomerModel
+
 
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/customersList', methods=['GET'])
-def home():
-	return jsonify (customersList), 200
+@app.route('/allCustomers', methods=['GET'])
+def getAllCustomers():
+	allCustomers = CustomerModel.query.all()
+	customerSchema = CustomerSchema(many=True)
+	outPut = customerSchema.dump(allCustomers)
+	return jsonify({'customers':outPut}), 200
 
-@app.route('/enviar.html', methods = ['GET'])
-def enviar():
-	return jsonify("Dados enviados com sucesso!"), 200 # ORGANIZAR TODO
-    #return render_template('index.html')
+@app.route('/allCustomers/<string:namePassed>', methods=['GET'])
+def getCustomerPerName(namePassed):
+	#specificCustomer = CustomerModel.query.filter(CustomerModel.name == namePassed)
+	specificCustomer = CustomerModel.query.filter(CustomerModel.name.like("%" + str(namePassed) + "%"), CustomerModel.historic == False)
+	customerSchema = CustomerSchema(many=True)
+	outPut = customerSchema.dump(specificCustomer)
+	return jsonify({'customer':outPut}), 200
 
-@app.route('/customersList/<string:name>', methods=['GET'])
-def customerPerName(name):
-	customerNames = []
-	for customer in customersList:
-		if customer['name'] == name:
-			customerNames += customer
-	if customerNames == []:
-		return jsonify ('Nenhum nome encontrado'), 401
-	return jsonify(customerNames), 200
+@app.route('/sendRegistry', methods=['POST'])
+def postCustomer():
+	engine = create_engine(config.connectionString)
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	data = request.get_json(True, True, True)
+	customerToAdd = CustomerModel(name = data['name'], birthDay = data['birthDay'], generalRegistry = data['generalRegistry'], address = data['address'], phoneNumber = data['phoneNumber'], historic = False) # Acabou de criar não é historico
+	session.add(customerToAdd)
+	session.commit()
+	return jsonify(customerToAdd.name), 201
 
-@app.route('/customersList/<int:id>', methods=['GET'])
-def customerPerId(id):
-	for customer in customersList:
-		if customer['id'] == id:
-			return jsonify (customer), 200
-	return jsonify ('Nenhum Id encontrado'), 401
-
-
-@app.route('/customersList', methods=['POST'])
-def saveCustomer():
-	dado = request.get_json(True, True, True)
-	customersList.append(dado) #verificar se registro já existe
-	return jsonify(customersList), 201
-
-@app.route('/customersList/<int:id>', methods=['DELETE'])
-def deleteCustomer(id):
-	# realizar deleção lógica
-	del customersList[id-1]
-	return jsonify(customersList), 200
 
 if __name__ == '__main__':
 	app.run(debug = True, host = '127.0.0.1')
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ port = '3306'
 user = 'hugo'
 password = 'pwd.123'
 database = 'registers_db'
+connectionString = 'mysql+pymysql://' + user + ':'+ password + '@' + host+ ':' + port + '/' + database
